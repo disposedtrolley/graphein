70bbf51df60f6927559abc254d06a60bfb69aa1e
James Liu <james@jamesliu.io>
2021-10-08 20:22:12 +1100

    Read and parse the React build manifest

--- src/commands/newMap.ts
+++ src/commands/newMap.ts
@@ -1,11 +1,14 @@
 import * as path from "path";
+import * as fs from "fs";
 import * as vscode from "vscode";
 
-interface newMapArgs {
-    extensionPath: string
+const REACT_BUILD_FOLDER = "react-build";
+
+interface NewMapArgs {
+  extensionPath: string;
 }
 
-export const newMap = (args: newMapArgs) => {
+export const newMap = (args: NewMapArgs) => {
   vscode.window.showInformationMessage("Hello World from graphein!");
 
   const panel = vscode.window.createWebviewPanel(
@@ -13,15 +16,40 @@ export const newMap = (args: newMapArgs) => {
     "Graphein - Map",
     vscode.ViewColumn.Two,
     {
-        enableScripts: true,
+      enableScripts: true,
+      localResourceRoots: [
+        vscode.Uri.file(path.join(args.extensionPath, REACT_BUILD_FOLDER)),
+      ],
     }
   );
 
   panel.webview.html = getWebviewContent();
+  console.log(getReactManifest(args.extensionPath));
+};
+
+interface ReactManifest {
+  mainJSPath: string;
+  mainCSSPath: string;
+  indexHTMLPath: string;
+}
+
+const getReactManifest = (basePath: string): ReactManifest => {
+  const buildPath = path.join(basePath, REACT_BUILD_FOLDER);
+  const manifestPath = path.join(buildPath, "asset-manifest.json");
+
+  const manifest = fs.readFileSync(manifestPath);
+  const manifestJSON = JSON.parse(manifest.toString());
+  const manifestJSONFiles = manifestJSON["files"];
+
+  return {
+    indexHTMLPath: path.join(buildPath, manifestJSONFiles["index.html"]),
+    mainJSPath: path.join(buildPath, manifestJSONFiles["main.js"]),
+    mainCSSPath: path.join(buildPath, manifestJSONFiles["main.css"]),
+  };
 };
 
 const getWebviewContent = () => {
-    return `<!DOCTYPE html>
+  return `<!DOCTYPE html>
       <html lang="en">
       <head>
           <meta charset="UTF-8">
@@ -32,5 +60,4 @@ const getWebviewContent = () => {
           <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
       </body>
       </html>`;
-      }
-}
+};
