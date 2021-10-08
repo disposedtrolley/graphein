d314da2a603e1cf7002559326a39519bf1d88c95
James Liu <james@jamesliu.io>
2021-10-09 10:16:49 +1100

    Remove unused crap, set up minimal digraph example

--- src-react/src/App.tsx
+++ src-react/src/App.tsx
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { GraphView } from "react-digraph";
+import React from "react";
+
+const GraphConfig = {
+  NodeTypes: {
+    empty: {
+      // required to show empty nodes
+      typeText: "None",
+      shapeId: "#empty", // relates to the type property of a node
+      shape: (
+        <symbol viewBox="0 0 100 100" id="empty" key="0">
+          <circle cx="50" cy="50" r="45"></circle>
+        </symbol>
+      ),
+    },
+  },
+  NodeSubtypes: {},
+  EdgeTypes: {
+    emptyEdge: {
+      // required to show empty edges
+      shapeId: "#emptyEdge",
+      shape: (
+        <symbol viewBox="0 0 50 50" id="emptyEdge" key="0">
+          <circle cx="25" cy="25" r="8" fill="currentColor">
+            {" "}
+          </circle>
+        </symbol>
+      ),
+    },
+  },
+};
 
 function App() {
+  const graph = {
+    nodes: [
+      {
+        id: 1,
+        title: "Node A",
+        x: 258.3976135253906,
+        y: 331.9783248901367,
+        type: "empty",
+      },
+      {
+        id: 2,
+        title: "Node B",
+        x: 593.9393920898438,
+        y: 260.6060791015625,
+        type: "empty",
+      },
+    ],
+    edges: [
+      {
+        source: 1,
+        target: 2,
+        type: "emptyEdge",
+      },
+    ],
+  };
+  const nodes = graph.nodes;
+  const edges = graph.edges;
+
+  const NodeTypes = GraphConfig.NodeTypes;
+  const NodeSubtypes = GraphConfig.NodeSubtypes;
+  const EdgeTypes = GraphConfig.EdgeTypes;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GraphView
+      ref={React.createRef()}
+      nodeKey="id"
+      nodes={nodes}
+      edges={edges}
+      nodeTypes={NodeTypes}
+      nodeSubtypes={NodeSubtypes}
+      edgeTypes={EdgeTypes}
+      allowMultiselect={false}
+    />
   );
 }
 
