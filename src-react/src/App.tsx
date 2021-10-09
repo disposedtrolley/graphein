31eb5203a3d5b71e49af5fc150305bc845e8286e
James Liu <james@jamesliu.io>
2021-10-09 11:28:43 +1100

    Rounded corners and custom node text rendering

--- src-react/src/App.tsx
+++ src-react/src/App.tsx
@@ -3,28 +3,21 @@ import React from "react";
 
 const GraphConfig = {
   NodeTypes: {
-    empty: {
-      // required to show empty nodes
-      typeText: "None",
-      shapeId: "#empty", // relates to the type property of a node
+    symbol: {
+      shapeId: "#symbol",
       shape: (
-        <symbol viewBox="0 0 100 100" id="empty" key="0">
-          <circle cx="50" cy="50" r="45"></circle>
+        <symbol viewBox="0 0 200 100" id="symbol" key="0">
+          <rect width="100%" height="100%" rx="15"></rect>
         </symbol>
       ),
     },
   },
   NodeSubtypes: {},
   EdgeTypes: {
-    emptyEdge: {
-      // required to show empty edges
-      shapeId: "#emptyEdge",
+    symbolConnection: {
+      shapeId: "#symbolConnection",
       shape: (
-        <symbol viewBox="0 0 50 50" id="emptyEdge" key="0">
-          <circle cx="25" cy="25" r="8" fill="currentColor">
-            {" "}
-          </circle>
-        </symbol>
+        <symbol viewBox="0 0 50 50" id="symbolConnection" key="0"></symbol>
       ),
     },
   },
@@ -38,21 +31,21 @@ function App() {
         title: "Node A",
         x: 258.3976135253906,
         y: 331.9783248901367,
-        type: "empty",
+        type: "symbol",
       },
       {
         id: 2,
         title: "Node B",
         x: 593.9393920898438,
         y: 260.6060791015625,
-        type: "empty",
+        type: "symbol",
       },
     ],
     edges: [
       {
         source: 1,
         target: 2,
-        type: "emptyEdge",
+        type: "symbolConnection",
       },
     ],
   };
@@ -73,6 +66,24 @@ function App() {
       nodeSubtypes={NodeSubtypes}
       edgeTypes={EdgeTypes}
       allowMultiselect={false}
+      renderNodeText={(
+        data: { title: string },
+        id: string | number,
+        isSelected: boolean
+      ) => {
+        return (
+          <text
+            ref={React.createRef()}
+            className="node-text"
+            text-anchor="middle"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <tspan font-size="10px" xmlns="http://www.w3.org/2000/svg">
+              {data.title}
+            </tspan>
+          </text>
+        );
+      }}
     />
   );
 }
